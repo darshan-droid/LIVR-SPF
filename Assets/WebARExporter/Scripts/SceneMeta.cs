ecc497b099e5161a7cc575ae7bc904e07458089f
Darshan <darshan@livr.co>
2025-10-29 16:57:01 +0000

    LIVR_WebGLPlugin to WebXR plugin
    Commenting all lines of LIVR_Webglplugin and transferrring it to WEBXR plugin.

--- Assets/WebARExporter/Scripts/SceneMeta.cs
+++ Assets/WebARExporter/Scripts/SceneMeta.cs
@@ -1,48 +1,48 @@
-using System;
-using System.Collections.Generic;
-using UnityEngine;
+//using System;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-namespace LivrWebARPlugin
-{
-    [Serializable]
-    public class SceneMeta
-    {
-        [Serializable]
-        public class ObjectInfo
-        {
-            //Objects from WebARBehaviour
-            public string name;
-            public string ContentID;
-            public bool PlaneOnSurface;
-            public bool AllowRotation;
-            public bool AllowScaling;
-            public float[] InitialScale;
-        }
-        public string SceneID;
-        public List<ObjectInfo> Objects = new();
-    }
-    public static class SceneMetaBuilder
-    {
-        public static SceneMeta BuildfromScene(string sceneID)
-        {            
-            var meta = new SceneMeta {SceneID = sceneID };      
-            foreach (var beh in GameObject.FindObjectsOfType<WebARBehaviour>(true))
-            {
-                meta.Objects.Add(new SceneMeta.ObjectInfo
-                {
-                    ContentID = beh.Content_Id,                    
-                    name = beh.gameObject.name,
-                    PlaneOnSurface = beh.PlaneOnSurface,
-                    AllowRotation = beh.AllowRotate,
-                    AllowScaling = beh.AllowScale,
-                    InitialScale = new float[]
-                        {beh.InitialScale.x, beh.InitialScale.y, beh.InitialScale.z}
-                });
-            }
-            return meta;
-        }
+//namespace LivrWebARPlugin
+//{
+//    [Serializable]
+//    public class SceneMeta
+//    {
+//        [Serializable]
+//        public class ObjectInfo
+//        {
+//            //Objects from WebARBehaviour
+//            public string name;
+//            public string ContentID;
+//            public bool PlaneOnSurface;
+//            public bool AllowRotation;
+//            public bool AllowScaling;
+//            public float[] InitialScale;
+//        }
+//        public string SceneID;
+//        public List<ObjectInfo> Objects = new();
+//    }
+//    public static class SceneMetaBuilder
+//    {
+//        public static SceneMeta BuildfromScene(string sceneID)
+//        {            
+//            var meta = new SceneMeta {SceneID = sceneID };      
+//            foreach (var beh in GameObject.FindObjectsOfType<WebARBehaviour>(true))
+//            {
+//                meta.Objects.Add(new SceneMeta.ObjectInfo
+//                {
+//                    ContentID = beh.Content_Id,                    
+//                    name = beh.gameObject.name,
+//                    PlaneOnSurface = beh.PlaneOnSurface,
+//                    AllowRotation = beh.AllowRotate,
+//                    AllowScaling = beh.AllowScale,
+//                    InitialScale = new float[]
+//                        {beh.InitialScale.x, beh.InitialScale.y, beh.InitialScale.z}
+//                });
+//            }
+//            return meta;
+//        }
 
-        public static string ToJson(SceneMeta meta) =>
-            JsonUtility.ToJson(meta, true);
-    }
-}  
+//        public static string ToJson(SceneMeta meta) =>
+//            JsonUtility.ToJson(meta, true);
+//    }
+//}  
